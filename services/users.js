28a7707e2f8738def72d15e91a9b080a5d2c5e14
Khalilur Rahman <khal.rahman@gmail.com>
2016-03-17 22:21:26 +0800

    added service

---
+++ services/users.js
@@ -0,0 +1,44 @@
+'use strict';
+ 
+var uuid = require('node-uuid');
+ 
+class UsersService {
+    constructor() {
+        this.Users = [];
+    }
+ 
+    getUsers() {
+        return this.Users;
+    }
+ 
+    addUser(user) {
+        if (!user || this.Users.filter(p => (p.name === user.name && p.email === user.email)).length > 0) {
+            return false;
+        }
+ 
+        user.id = uuid.v4();
+ 
+        this.Users.push(user);
+        return true;
+    }
+
+    getSingleUser(userId) {
+        var user = this.Users.filter(p => p.id === userId)[0];
+ 
+        return user || null;
+    }
+ 
+    updateUser(userId, data) {
+        var user = this.getSingleUser(userId);
+        if (user) {
+            user.name = data.name ? data.name : user.name;
+            user.email = data.email ? data.email : user.email;
+ 
+            return true;
+        }
+        return false;
+    }
+
+}
+ 
+module.exports = new UsersService();
---
+++ test/services/users.js
@@ -0,0 +1,55 @@
+var UsersService = require('../../services/users');
+
+describe('UsersService', function() {
+	describe('#getUsers()', function() {
+		it('should return an array without error', function(done) {
+			if (!Array.isArray(UsersService.getUsers()))
+				throw err;
+			done();
+		});
+	});
+});
+
+describe('UsersService', function() {
+	describe('#addUser(user)', function() {
+		it('should add user without error', function(done) {
+			if (!UsersService.addUser({name: 'khal', email: 'khal.rahman@gmail.com'}))
+				throw err;
+			done();
+		});
+	});
+});
+
+describe('UsersService', function() {
+	var data  = {name: 'khal', email: 'khal.rahman@gmail.com'};
+	beforeEach(function() {
+		return UsersService.addUser(data);
+	});
+	describe('#getSingleUser(userId)', function() {
+		it('should find user by id without error', function(done) {
+			var foundUser = UsersService.getSingleUser(UsersService.getUsers()[0].id);
+			if (!foundUser || foundUser == null)
+				throw err;
+			done();
+		});
+	});
+});
+
+describe('UsersService', function() {
+	beforeEach(function() {
+		var data = {name: 'khal', email: 'khal.rahman@gmail.com'};
+		return UsersService.addUser(data);
+	});
+	describe('#updateUser(userId, data)', function() {
+		it('should update user without error', function(done) {
+			var foundUser = UsersService.getSingleUser(UsersService.getUsers()[0].id);
+
+			// update name
+			UsersService.updateUser(foundUser.id, {name: 'nevda', email: 'khal.rahman@gmail.com'});
+			if (foundUser.name !== 'nevda') {
+				throw err;
+			}
+			done();
+		});
+	});
+});
